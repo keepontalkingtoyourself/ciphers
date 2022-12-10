f7f1d8b014e56bfdfd63f8e19c9b8c9baa95077c
keepontalkingtoyourself <>
2022-12-10 19:04:09 +0300

    Добавил возможность показать расшифрованные сообщения

--- test_1/caesar.py
+++ test_1/caesar.py
@@ -14,16 +14,25 @@ def get_key(plain_text, encrypted_text):
 
 def main():
     print("«Шифр Цезаря")
-    action = int(input("[1] Показать все шифровки:\n[2] Найти ключ:\n→ "))
+    action = int(input("[1] Шифровки:\n[2] Расшифровки:\n[3] Найти ключ:\n→ "))
 
     if action == 1:
         message = input("Текст:\n→ ")
+        print()
         for key in range(34):
             print(key, encrypt(message, key))
+
     elif action == 2:
+        message = input("Зашифрованный текст:\n→ ")
+        print()
+        for key in range(33, -1, -1):
+            print(33 - key, encrypt(message, key))
+
+    elif action == 3:
         plain_text = input("Исходный текст:\n→ ")
         encrypted_text = input("Зашифрованный текст:\n→ ")
-        print(get_key(plain_text, encrypted_text))
+        print()
+        print(f"Результат:\n{get_key(plain_text, encrypted_text)}")
 
 
 if __name__ == '__main__':
